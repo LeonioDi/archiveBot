87fd1cfaa3bef7e39bf892e7a041f7798f321e97
LeonioDi <LeonioDi>
2019-04-10 11:24:32 +0300

    Рефактор работы с бд, добавил декоратор @connect

--- workToDB.py
+++ workToDB.py
@@ -1,23 +1,32 @@
 import sqlite3
 
-""" Добавление нового пользователя """
+
+def connect(func):
+    def connectTo(*args, **kwargs):
+        try:
+            con = sqlite3.connect("db_archive")
+            cur = con.cursor()
+        except Exception as e:
+            print(e)
+        return func(con, cur, *args, **kwargs)
+
+    return connectTo
 
 
-def addUser(idUser, name):
-    con = sqlite3.connect("db_archive")
-    cur = con.cursor()
+""" Метод обавления нового пользователя """
+
+
+@connect
+def addUser(con, cur, idUser, name):
     cur.execute("INSERT INTO USER(idUser, name) VALUES ({},'{}')".format(idUser, name))
     con.commit()
-    cur.close()
-    con.close()
 
 
 """ Метод Проверки пользователя на верификацию """
 
 
-def chekUser(idUser):
-    con = sqlite3.connect("db_archive")
-    cur = con.cursor()
+@connect
+def chekUser(con, cur, idUser):
     cur.execute("SELECT verification FROM USER WHERE idUser = {}".format(idUser))
     con.commit()
     try:
@@ -25,8 +34,6 @@ def chekUser(idUser):
     except Exception as e:
         print(e)
         res = None
-    cur.close()
-    con.close()
     if res is not None:
         if res[0] == 'TRUE':
             return True
@@ -39,94 +46,73 @@ def chekUser(idUser):
 """Методы для работы мастаера добавления статьи """
 
 
-def getCurrentState(idUser):
-    con = sqlite3.connect("db_archive")
-    cur = con.cursor()
+@connect
+def getCurrentState(con, cur, idUser):
     cur.execute("SELECT currentState FROM USER WHERE idUser = {}".format(idUser))
     con.commit()
     try:
         res = cur.fetchall()[0]
+        return res[-1]
     except Exception as e:
         print(e)
-        res = None
-    cur.close()
-    con.close()
-    return res[-1]
+        return None
 
 
-def setCurrentState(idUser, value):
-    con = sqlite3.connect("db_archive")
-    cur = con.cursor()
+@connect
+def setCurrentState(con, cur, idUser, value):
     cur.execute("UPDATE USER SET currentState = {} WHERE idUser = {}".format(value, idUser))
     con.commit()
-    cur.close()
-    con.close()
 
 
 """ методы для определения и работы с активной статьей"""
 
 
-def getCurrentidArticle(idUser):
-    con = sqlite3.connect("db_archive")
-    cur = con.cursor()
+@connect
+def getCurrentidArticle(con, cur, idUser):
     cur.execute("SELECT currentidArticle FROM USER WHERE idUser = {}".format(idUser))
     con.commit()
     try:
         res = cur.fetchall()[0]
+        return res[-1]
     except Exception as e:
         print(e)
-        res = None
-    cur.close()
-    con.close()
-    return res[-1]
+        return None
 
 
-def setCurrentidArticle(idUser, idArticle):
-    con = sqlite3.connect("db_archive")
-    cur = con.cursor()
+@connect
+def setCurrentidArticle(con, cur, idUser, idArticle):
     cur.execute("UPDATE USER SET currentidArticle = '{}' WHERE idUser = {}".format(idArticle, idUser))
     con.commit()
-    cur.close()
-    con.close()
 
 
 """Добавление новой статьи"""
 
 
-def addArticle(idArticle, idUser):
-    con = sqlite3.connect("db_archive")
-    cur = con.cursor()
+@connect
+def addArticle(con, cur, idArticle, idUser):
     cur.execute("INSERT INTO ARTICLE(idArticle, idUser) VALUES ('{}','{}')".format(idArticle, idUser))
     con.commit()
-    cur.close()
-    con.close()
 
 
 """Обновление данных статьи по ключу"""
 
 
-def updateArticle(idArticle, key, value):
-    con = sqlite3.connect("db_archive")
-    cur = con.cursor()
+@connect
+def updateArticle(con, cur, idArticle, key, value):
     cur.execute("UPDATE ARTICLE SET {} = '{}' WHERE idArticle = '{}'".format(key, value, idArticle))
     con.commit()
-    cur.close()
-    con.close()
 
 
 """Получение всех данных по статье"""
 
 
-def getAllArticle(idArticle):
-    con = sqlite3.connect("db_archive")
-    cur = con.cursor()
+@connect
+def getAllArticle(con, cur, idArticle):
     cur.execute("SELECT * FROM ARTICLE WHERE idArticle = '{}'".format(idArticle))
     con.commit()
     try:
         res = cur.fetchall()[0]
+        return res
     except Exception as e:
         print(e)
-        res = None
-    cur.close()
-    con.close()
-    return res
+        return None
